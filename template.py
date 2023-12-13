bc911305bdc59637e14bebc4f7f7cfc64a2a55bd
hebollae <Henri.Bollaert@UGent.be>
2023-12-13 16:00:24 +0100

    template update

--- template.py
+++ template.py
@@ -6,19 +6,11 @@ DAY = "00"
 def parse(lines: list[str]):
     pass
 
-def part1(lines: list[str]) -> str:
-    start = timer()
+def part1(lines: list[str]):
+    pass
 
-    end = timer()
-
-    print(f'In {(end - start)*1000}ms.')
-
-def part2(lines: list[str]) -> str:
-    start = timer()
-
-    end = timer()
-
-    print(f'In {(end - start)*1000}ms.')
+def part2(lines: list[str]):
+    pass
 
 def main():
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -27,8 +19,12 @@ def main():
     lines = [line.strip() for line in f.readlines()]
     f.close()
 
-    print("Score for part 1:", part1(lines))
-    print("Score for part 2:", part2(lines))
+    for nr, function in enumerate([part1, part2]):
+        start = timer()
+        result = function(lines)
+        end = timer()
+        print(f"Score for part {nr + 1}: {result} in {(end - start)*1000}ms.")
+
 
 if __name__ == "__main__":
     main()
